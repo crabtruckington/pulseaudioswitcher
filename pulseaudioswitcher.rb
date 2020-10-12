593a74a5ac1b05b306ddd13bc4b7f967ad264cae
crabtruckington <47725554+crabtruckington@users.noreply.github.com>
2020-10-12 16:09:53 -0400

    Update pulseaudioswitcher.rb

--- pulseaudioswitcher.rb
+++ pulseaudioswitcher.rb
@@ -15,27 +15,21 @@ devices = bash(cmd)
 
 
 devices.each_line do |line|
-    puts line
     deviceDetails = line.chomp.split("=")
     @deviceList[deviceDetails[0]] = deviceDetails[1]
     if (deviceDetails[2] == "RUNNING")
         @activeDevice = deviceDetails[0]
-        puts deviceDetails[1] + "is the active device"
     end
 end
 
 puts @deviceList
 
 if @activeDevice == "1"
-    puts "Setting device 2 to be the active device"
     cmd = "pactl set-default-sink '#{@deviceList["2"]}'"
-    puts cmd
     bash(cmd)
     bash("aplay /pulseaudioswitcher/beep.wav")
 else
-    puts "Setting device 1 to be the active device"
     cmd = "pactl set-default-sink '#{@deviceList["1"]}'"
-    puts cmd
     bash(cmd)
     bash("aplay /pulseaudioswitcher/beep.wav")
 end
